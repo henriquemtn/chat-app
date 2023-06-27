74e6750ae93a4fdf41c68a101659c96b7a517f0e
Henrique Silveira <92762031+henriquemtn@users.noreply.github.com>
2023-06-27 01:53:37 -0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-    apiKey: "#",
-    authDomain: "#",
-    projectId: "#",
-    storageBucket: "#",
-    messagingSenderId: "#",
-    appId: "#"
+    apiKey: "AIzaSyAa-UCUfrD4xY-u7EGQMs0Sf-AUE-b2384",
+  authDomain: "chat-app-f742a.firebaseapp.com",
+  projectId: "chat-app-f742a",
+  storageBucket: "chat-app-f742a.appspot.com",
+  messagingSenderId: "788340022795",
+  appId: "1:788340022795:web:6f1aa00be019d85f51b10e"
   };
 
   const app = firebase.initializeApp(firebaseConfig);
